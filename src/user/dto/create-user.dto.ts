1bece86c5cb5bb253ee1152e138a97ae4c3283a7
Jose Guilherme <jgnvs6@gmail.com>
2021-11-19 17:03:45 -0300

    feat(feature/add-documentation): add user schemas

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,23 +1,28 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Transform } from "class-transformer";
 import { IsEmail, IsNotEmpty, IsOptional, IsString } from "class-validator";
 
 
 export class CreateUserDto {
   
+  @ApiProperty({ required: true })
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({ required: true })
   @IsNotEmpty()
   @IsString()
   @Transform(({ value }) => (typeof value === "string")? value?.trim() : value)
   password: string;
 
+  @ApiProperty({ required: true })
   @IsNotEmpty()
   @IsString()
   @Transform(({ value }) => (typeof value === "string")? value?.trim() : value)
   name: string;
 
+  @ApiProperty({ required: false })
   @IsOptional()
   @IsString()
   @Transform(({ value }) => (typeof value === "string")? value?.trim() : value)
