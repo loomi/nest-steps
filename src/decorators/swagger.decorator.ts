1c205af5f2cd70f9f66f835d879f34624a575995
Jose Guilherme <jgnvs6@gmail.com>
2021-11-19 17:08:42 -0300

    feat(feature/add-documentation): swagger global decorator

---
+++ src/decorators/swagger.decorator.ts
@@ -0,0 +1,14 @@
+import { applyDecorators } from '@nestjs/common';
+import { ApiResponse } from '@nestjs/swagger';
+
+export function GlobalResponseSwagger() {
+  return applyDecorators(
+    ApiResponse({ status: 200, description: 'OK'}),
+    ApiResponse({ status: 201, description: 'CREATED'}),
+    ApiResponse({ status: 204, description: 'NO CONTENT'}),
+    ApiResponse({ status: 400, description: 'BAD REQUEST'}),
+    ApiResponse({ status: 404, description: 'NOT FOUND'}),
+    ApiResponse({ status: 409, description: 'CONFLICT'}),
+    ApiResponse({ status: 500, description: 'SERVER ERROR'}),
+  );
+}
