96fa668c13b047641d756da9443bbecadaff96ee
Jose Guilherme <jgnvs6@gmail.com>
2021-11-19 16:20:19 -0300

    fix(develop): add 'id' field in ListUserDto

--- src/user/dto/list-user.dto.ts
+++ src/user/dto/list-user.dto.ts
@@ -1,6 +1,11 @@
-import { IsOptional, IsString } from "class-validator";
+import { IsOptional, IsString, IsUUID } from "class-validator";
 
 export class ListUserDto {
+  
+  @IsOptional()
+  @IsUUID()
+  id: string;
+
   @IsOptional()
   @IsString()
   email: string;
