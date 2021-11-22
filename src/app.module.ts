8858f1606cb467de4c6c4eed35dc83d86f278d08
Jose Guilherme <jgnvs6@gmail.com>
2021-11-22 10:14:04 -0300

    feat(feature/add-prisma-orm): create database resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { UserModule } from './user/user.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), UserModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), UserModule, DatabaseModule],
   controllers: [],
   providers: [],
 })
