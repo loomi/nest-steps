0833f4e4b089894517dc348e6d126a2d33b01d9c
Jose Guilherme <jgnvs6@gmail.com>
2021-12-10 08:07:55 -0300

    feat(develop): add authorization guard

---
+++ src/shared/guards/roles.guard.ts
@@ -0,0 +1,11 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    // TODO implement authorization
+    return true;
+  }
+}
